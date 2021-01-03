3c653ef0b94761030b32049c5cebad322b52dad0
MathisPr <mathis_prange@hotmail.com>
2021-01-03 17:45:44 +0100

    Create app.js

---
+++ app.js
@@ -0,0 +1,9 @@
+//Define UI Variables
+const form          = document.querySelector('#task-form');
+const tasksList     = document.querySelector('.collection');
+const clearBtn      = document.querySelector('.clears-tasks');
+const filter        = document.querySelector('#filter');
+const taskInput     = document.querySelector('#task');         
+
+
+
